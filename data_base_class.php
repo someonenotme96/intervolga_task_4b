a1cd4ba93d893fe4ac395c5bd8b106c295e928ba
someonenotme96 <53169932+someonenotme96@users.noreply.github.com>
2019-07-22 16:41:26 +0300

    Update data_base_class.php

--- data_base_class.php
+++ data_base_class.php
@@ -2,27 +2,28 @@
 class DataBase {
     
     public $DBH;
-    
+    //Подключение к db
     public function __construct()
-    {
-    
-    $user = "root";
-    $pass = "";
-    $this->$DBH = new PDO('mysql:host=localhost;dbname=countries_db','root',''); 
-    }
+        {
+            $user = "root";
+            $pass = "";
+            $this->$DBH = new PDO('mysql:host=localhost;dbname=countries_db','root',''); 
+        }
+    //Запрос получения всех данных из таблицы стран
     public function GetAll()
         {
             return $this->$DBH->query('SELECT * FROM `countries`');
             
         }
-    
+    //Запрос получения id страны по имени, для проверки присутствия в таблице
     public function GetAllByCountryName($country_name)
         {
-            $stmt = $this->$DBH->prepare('SELECT capital FROM `countries` WHERE country_name= :name');
+            $stmt = $this->$DBH->prepare('SELECT country_id FROM `countries` WHERE country_name= :name');
             $stmt->execute([':name'=> $country_name]);
             $res = $sdd->fetch(PDO::FETCH_BOTH);
             return $res;
         }
+        //Запрос добавления новой страны
     public function AddCountry($country_name, $country_capital, $country_president) {
 
         if($this->GetAllByCountryName($country_name) != null)
